46b9690dab5bbc8522dccdc10df01feb1560f206
Jeff T <tarutmauktoe@gmail.com>
2018-03-02 14:07:38 -0600

    Implement routing

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       posts: [],
     }
---
+++ src/components/UploadImage.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import Dropzone from 'react-dropzone';
+import axios from 'axios'
+
+export default class UploadImage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      uploadedFileCloudinaryUrl: ''
+    };
+  }
+
+  onImageDrop(files) {
+    this.setState({
+      uploadedFile: files[0]
+    });
+
+//    this.handleImageUpload(files[0]);
+    this.fileUploadHandler();
+  }
+
+  fileUploadHandler = () => {
+    console.log(this.uploadedFile);
+  }
+
+/*
+  handleImageUpload(file) {
+    let upload = request.post(CLOUDINARY_UPLOAD_URL)
+                        .field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
+                        .field('file', file);
+
+    upload.end((err, response) => {
+      if (err) {
+        console.error(err);
+      }
+
+      if (response.body.secure_url !== '') {
+        this.setState({
+          uploadedFileCloudinaryUrl: response.body.secure_url
+        });
+      }
+    });
+  }
+*/
+
+  render() {
+    return (
+      <form>
+        <div className="FileUpload">
+          <Dropzone
+            multiple={false}
+            accept="image/*"
+            onDrop={this.onImageDrop.bind(this)}>
+            <p>Drop an image or click to select a file to upload.</p>
+          </Dropzone>
+        </div>
+
+        <div>
+          {this.state.uploadedFileCloudinaryUrl === '' ? null :
+          <div>
+            <p>{this.state.uploadedFile.name}</p>
+            <img src={this.state.uploadedFileCloudinaryUrl} />
+          </div>}
+        </div>
+      </form>
+    );
+  }
+}
