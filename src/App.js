3835a328484431d226affff83196afca04774e27
Jeff T <tarutmauktoe@gmail.com>
2018-03-02 18:07:02 -0600

    Remove duplicate upload code

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ class App extends Component {
     super(props);
     this.state = {
       posts: [],
-      selectedFile: null
     }
   }
 
@@ -29,54 +28,6 @@ class App extends Component {
     }).catch((error) => {
         console.log(error);
     });
-  }
-
-  fileSelectedHandler = event => {
-    this.setState({
-      selectedFile:event.target.files[0]
-    });
-  }
-
-  fileUploadHandler = () => {
-    const files = this.filesInput.files;
-
-    var imageMimeTypes = ['image/jpg' , 'image/jpeg', 'image/png', 'image/bmp', 'image/gif'];
-
-    for (var key in files) {
-    // check if this is a file:
-      if (files.hasOwnProperty(key) && files[key] instanceof File) {
-        const file = this.filesInput.files[key];
-        // check if this is an image:
-        if (imageMimeTypes.indexOf(file.type) !== -1) {
-          const reader = new FileReader();
-          reader.readAsBinaryString(file);
-
-          reader.onload = function() {
-            axios.post('http://localhost:8095/users/1/posts', { image: btoa(reader.result) }, {
-              onUploadProgress: progressEvent => {
-                console.log('Upload Progress: ' + Math.round((progressEvent.loaded/progressEvent.total) * 100) + '%')
-              }
-            })
-            .then(res => {
-              console.log(res);
-            });
-          };
-          reader.onerror = function() {
-              console.log('there are some problems');
-          };
-
-          /*
-          console.log("key:", key);
-          for (var key2 in files[key]) {
-            console.log("key2:", key2, " value:", files[key][key2]);
-            for (var key3 in files[key][key2]) {
-              console.log("key3:", key3, " value:", files[key][key2][key3]);
-            }
-          }
-          */
-        }
-      }
-    }
   }
 
   render() {
