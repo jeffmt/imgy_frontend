176035d19a94359ad63cfc7ae3776a9fd25d7d5c
Jeff T <tarutmauktoe@gmail.com>
2018-03-16 22:50:15 -0500

    Add Popover when mouse over images

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
-import {Grid, Col, Row, Image} from 'react-bootstrap';
+import {Grid, Col, Row, Image, OverlayTrigger, Popover} from 'react-bootstrap';
 
 class Posts extends Component {
   render() {
     let postItems;
     if(this.props.posts){
       postItems = this.props.posts.map(post => {
+        let {id, description, image, views} = post;
 
-        const src = "data:image/jpg;base64," + post.image;
+        description = description || " ";
 
+        const src = "data:image/jpg;base64," + image;
+
+        const popover = (
+          <Popover id={id} title={description}>
+            {views} views
+          </Popover>
+        );
         return (
-            <Col key={post.id} xs={12} md={4}>
-              <Image src={src} thumbnail />
+            <Col key={id} xs={12} md={4}>
+              <OverlayTrigger placement="left" overlay={popover}>
+                <a href="#"><Image src={src} thumbnail /></a>
+              </OverlayTrigger>
             </Col>
         );
       });
